9dd4344f35df4ff0f6905b9f1016adb83e347272
Vusal Huseynov <vslh333222111@gmail.com>
2025-03-23 00:49:37 +0400

    Container With Most Water

---
+++ Two Pointers/Container With Most Water.py
@@ -0,0 +1,16 @@
+from typing import List
+
+def maxArea(self, arr: List[int]) -> int:
+    l = 0
+    r = len(arr) - 1
+    m = 0
+
+    while(l < r):
+        if(arr[l] > arr[r]):
+            m = max(m,arr[r] * (r - l))
+            r -= 1
+        else:
+            m = max(m,arr[l] * (r - l))
+            l += 1
+            
+    return m
